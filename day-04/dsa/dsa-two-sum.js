073eefdb4a6325b93855c429952421a80c4b56d5
Zohaib-Qaisar-Kayani-ZQK <zqk.dev@gmail.com>
2026-08-06 23:52:54 +0500

    Day 04 - DSA: Two Sum

--- day-04/dsa/dsa-two-sum.js
+++ day-04/dsa/dsa-two-sum.js
@@ -3,6 +3,11 @@ const target = 9
 
 const twoSum = (arr,target) => {
     for (let i = 0; i<arr.length; i++) {
-        
+        if(arr[i] === target) {
+            return [i];
+        }
     }
+    return -1;
 }
+
+console.log(twoSum(arr,target));
